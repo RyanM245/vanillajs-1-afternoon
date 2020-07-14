3bb27f4023437835710ff101b5d9590697c1779e
Ryan Milne <ryanmilne101@gmail.com>
2020-07-14 15:51:46 -0600

    updated

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,14 @@
-console.log('The house always wins!')
+// console.log('The house always wins!')
+
+let idInput = document.getElementById("idInput")
+let colorInput = document.getElementById("colorInput")
+
+// console.log(idInput)
+// console.log(colorInput)
+
+
+function setCard(){
+    const card = document.getElementById(idInput.value)
+    // console.log(card)
+    card.style.color = colorInput.value
+}
